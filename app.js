6b5d3d23663fdfb97d1ad5351d322bdc29c0bb71
Eugenia Park <Eugenialpark@gmail.com>
2018-12-24 03:13:35 -0800

    complete (forgot to submit)

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-$(document).ready(function(){
-  console.log('jQuery loaded');
+$(document).ready(function(){  //when document's ready, run code
+  var groceryArr = [];  //array that holds list of groceries, default is empty
+  var parsedList = JSON.parse(localStorage.getItem('groceryKey'));  //parsed bc localstorage only
 
-  // write to local storage from input when button save clicked
-  $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
+  // checks if there are items in the local storage
+  if (localStorage.getItem('groceryKey')) {  //if there are items in the grocery list
+    groceryArr = parsedList;  //retrieve from localStorage and set as the grocery list;
+  }
 
-    // display the value here
-    $('.list-display-field').text(myItemInStorage); // ??
+  // when an input item is added/"add item" button clicked
+  $('.btn-submit').on('click', function(){    // write to local storage from input when add button clicked
+    groceryArr.push($('.grocery-item').val());  // adds input item to the grocery array list
+    localStorage.setItem('groceryKey', JSON.stringify(groceryArr));  //setItem(key, val) - store groceryArr into localStorage
 
+    $('.list-display-field').append("<li>" + groceryArr.slice(-1) + "</li>");  // appends the item to the DOM (adds item to the botton of the list)
+    $('.grocery-item').val('');  //resets input box after new item added/button clicked
   });
 
-  // delete from local storage when delete button clicked
+  // holds items in list when the page is refreshed
+  $('.body').ready(function() {    // when body's ready, run the code
+    if (groceryArr.length > 0) {  // if there's items in the array
+      for(var i = 0; i < parsedList.length; i++) {  //retrieve the list from localStorage
+        $('.list-display-field').append("<li>" + parsedList[i] + "</li>");  //add them to the DOM
+      }
+    }
+  });
+
+  // delete all items from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
-    localStorage.removeItem('inputFieldValue');
+    localStorage.clear();   //clears localStorage
+    $('li').remove();     //deletes all items from the DOM
   });
-
 });
+
+
+/* localStorage
+ .setItem('key', 'value') - Add key and value to local storage
+ .getItem() - retrieve value by key
+ .removeItem() - Remove an item by key
+ .clear() - Clear all storage
+*/
